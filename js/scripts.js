ff83b09b6c689f68464f01c1d65189972346d6e7
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-08-23 13:11:12 +0300

    Changed the name of the valid month and valid date functions and created variables validMonth and validDate

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,30 @@ function akanName() {
     var yearOFBirth = document.getElementById("put-year").value;
     var monthOfBirth = document.getElementById("put-month").value;
     var dateOfBirth = document.getElementById("put-date").value;
-    let gender = document.getElementsByName("gender")
+    let gender = document.getElementsByName("gender").value;
 
 }
+function GENDER() {
+    if (gender.checked) {
+        return genderValue;
+    }
+}
+let genderValue = GENDER();
+
+function monthValid() {
+    if (monthOfBirth <= 0 || monthOfBirth > 12) {
+        return invalid;
+    } else {
+        return valid;
+
+    }
+};
+function dateValid() {
+    if (dateOfBirth <= 0 || dateOfBirth > 31) {
+        return invalid;
+    } else {
+        return valid;
+    }
+};
+let validMonth = monthValid();
+let validDate = dateValid();
