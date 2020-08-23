9e2ebb9db66926c72ebc13b22e43598537703513
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-08-23 13:31:41 +0300

    Created variables for days of the week, male and female Akan names

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,13 @@ d.setFullYear();
 document.getElementsByClassName("inputs");
 
 let dayOfTheWeek = d;
+
+let daysofTheWeek = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+]
+let maleAkanNames = [
+    "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+]
+let femaleAkanNames = [
+    "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+]
