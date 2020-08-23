68ae6b7d166d3d490b1fb9dcfbee14d11ca51d86
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-08-23 13:07:06 +0300

    Created a function for Gender, valid-month and valid-date

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ function akanName() {
     var yearOFBirth = document.getElementById("put-year").value;
     var monthOfBirth = document.getElementById("put-month").value;
     var dateOfBirth = document.getElementById("put-date").value;
+    let gender = document.getElementsByName("gender")
 
 }
